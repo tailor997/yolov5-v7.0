c470f2f97b6910d5c18c746547f8fa261dcf516c
zhangzhe <zhangzhe@panchip.com>
2023-12-01 09:33:00 +0800

    update export func
    ref luban cat doc
    for rknn

--- models/yolo.py
+++ models/yolo.py
@@ -53,30 +53,36 @@ class Detect(nn.Module):
         self.m = nn.ModuleList(nn.Conv2d(x, self.no * self.na, 1) for x in ch)  # output conv
         self.inplace = inplace  # use inplace ops (e.g. slice assignment)
 
-    def forward(self, x):
+    # def forward(self, x):
+    #     z = []  # inference output
+    #     for i in range(self.nl):
+    #         x[i] = self.m[i](x[i])  # conv
+    #         bs, _, ny, nx = x[i].shape  # x(bs,255,20,20) to x(bs,3,20,20,85)
+    #         x[i] = x[i].view(bs, self.na, self.no, ny, nx).permute(0, 1, 3, 4, 2).contiguous()
+    #
+    #         if not self.training:  # inference
+    #             if self.dynamic or self.grid[i].shape[2:4] != x[i].shape[2:4]:
+    #                 self.grid[i], self.anchor_grid[i] = self._make_grid(nx, ny, i)
+    #
+    #             if isinstance(self, Segment):  # (boxes + masks)
+    #                 xy, wh, conf, mask = x[i].split((2, 2, self.nc + 1, self.no - self.nc - 5), 4)
+    #                 xy = (xy.sigmoid() * 2 + self.grid[i]) * self.stride[i]  # xy
+    #                 wh = (wh.sigmoid() * 2) ** 2 * self.anchor_grid[i]  # wh
+    #                 y = torch.cat((xy, wh, conf.sigmoid(), mask), 4)
+    #             else:  # Detect (boxes only)
+    #                 xy, wh, conf = x[i].sigmoid().split((2, 2, self.nc + 1), 4)
+    #                 xy = (xy * 2 + self.grid[i]) * self.stride[i]  # xy
+    #                 wh = (wh * 2) ** 2 * self.anchor_grid[i]  # wh
+    #                 y = torch.cat((xy, wh, conf), 4)
+    #             z.append(y.view(bs, self.na * nx * ny, self.no))
+    #
+    #     return x if self.training else (torch.cat(z, 1), ) if self.export else (torch.cat(z, 1), x)
+    def forward(self,x):
         z = []  # inference output
         for i in range(self.nl):
-            x[i] = self.m[i](x[i])  # conv
-            bs, _, ny, nx = x[i].shape  # x(bs,255,20,20) to x(bs,3,20,20,85)
-            x[i] = x[i].view(bs, self.na, self.no, ny, nx).permute(0, 1, 3, 4, 2).contiguous()
+            z.append(torch.sigmoid(self.m[i](x[i])))
 
-            if not self.training:  # inference
-                if self.dynamic or self.grid[i].shape[2:4] != x[i].shape[2:4]:
-                    self.grid[i], self.anchor_grid[i] = self._make_grid(nx, ny, i)
-
-                if isinstance(self, Segment):  # (boxes + masks)
-                    xy, wh, conf, mask = x[i].split((2, 2, self.nc + 1, self.no - self.nc - 5), 4)
-                    xy = (xy.sigmoid() * 2 + self.grid[i]) * self.stride[i]  # xy
-                    wh = (wh.sigmoid() * 2) ** 2 * self.anchor_grid[i]  # wh
-                    y = torch.cat((xy, wh, conf.sigmoid(), mask), 4)
-                else:  # Detect (boxes only)
-                    xy, wh, conf = x[i].sigmoid().split((2, 2, self.nc + 1), 4)
-                    xy = (xy * 2 + self.grid[i]) * self.stride[i]  # xy
-                    wh = (wh * 2) ** 2 * self.anchor_grid[i]  # wh
-                    y = torch.cat((xy, wh, conf), 4)
-                z.append(y.view(bs, self.na * nx * ny, self.no))
-
-        return x if self.training else (torch.cat(z, 1), ) if self.export else (torch.cat(z, 1), x)
+        return z
 
     def _make_grid(self, nx=20, ny=20, i=0, torch_1_10=check_version(torch.__version__, '1.10.0')):
         d = self.anchors[i].device
